35d3009e4a9882126d84003428e8ae032a318481
EnsoSantos <162356579+EnsoSantos@users.noreply.github.com>
2025-05-15 00:09:26 +0800

    fixed bugs

--- ledger/models.py
+++ ledger/models.py
@@ -17,7 +17,7 @@ class Recipe(models.Model):
         return self.name
         
     def get_absolute_url(self) :
-        return reverse('recipe_detail', args=[self.pk])
+        return reverse('ledger:recipe_detail', args=[self.pk])
     
 class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=100) 

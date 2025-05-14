934c907e0ef00fa8274dc5c60eff8fa2d4ea81f9
EnsoSantos <162356579+EnsoSantos@users.noreply.github.com>
2025-05-14 23:13:51 +0800

    updated recipes_detail views and template

--- ledger/views.py
+++ ledger/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 from .models import Recipe, Ingredient
 
-def recipe_list(request):
+def recipes_list(request):
     recipes = Recipe.objects.all()
     ctx = {
         "recipes": recipes
     }
-    return render(request, "ledger/recipe_list.html", ctx)
+    return render(request, "ledger/recipes_list.html", ctx)
 
 def recipe_detail(request, pk):
     recipe = Recipe.objects.get(pk=pk)
-    ingredients = recipe.ingredients.all()
+    ingredients = Ingredient.objects.filter(recipeingredient__recipe=recipe)
     ctx = {
         "recipe": recipe,
         "ingredients": ingredients

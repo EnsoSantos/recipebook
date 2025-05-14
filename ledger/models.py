0a8edeb101dee2e47ecc5de3bac38a82c3d9c470
EnsoSantos <162356579+EnsoSantos@users.noreply.github.com>
2025-05-14 22:19:53 +0800

    crated models

--- ledger/models.py
+++ ledger/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100) 
+
+    def __str__(self):
+        return self.name
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100) 
+
+    def __str__(self):
+        return self.name
+    
+class RecipeIngredient(models.Model):
+    quantity = models.CharField(max_length=100) 
+    ingredient = models.ForeignKey(Ingredient, null=True, on_delete=models.SET_NULL, related_name='recipe_ingredients')
+    recipe = models.ForeignKey(Recipe, null=True, on_delete=models.SET_NULL, related_name='ingredients')
+
+    def __str__(self):
+        return f"{self.quantity} {self.ingredient.name} in {self.recipe.name}"

c774866df9e1a81ae3e7618a58ff178b39f83f3c
EnsoSantos <162356579+EnsoSantos@users.noreply.github.com>
2025-05-23 11:09:43 +0800

    added profile model

--- ledger/models.py
+++ ledger/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    bio = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100) 

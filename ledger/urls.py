4bb2ee36b56ec414ba0d2a924530644220070398
EnsoSantos <162356579+EnsoSantos@users.noreply.github.com>
2025-05-23 11:56:43 +0800

    added login url

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import recipes_list, recipe_detail
+from . import views
 
 urlpatterns = [
     path('', recipes_list, name='recipes_list'),
     path('recipes/list/', recipes_list, name="recipes_list"),
-    path("recipe/<int:pk>/", recipe_detail, name="recipe_detail")
+    path("recipe/<int:pk>/", recipe_detail, name="recipe_detail"),
+    path('login/', views.login_view, name='login'),
 ]
 
 

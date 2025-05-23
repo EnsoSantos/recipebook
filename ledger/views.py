3d4760e0bf2177805a16f53703f84b8b5a635aae
EnsoSantos <162356579+EnsoSantos@users.noreply.github.com>
2025-05-23 11:53:09 +0800

    added login view

--- ledger/views.py
+++ ledger/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from .models import Recipe, RecipeIngredient
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
 
 @login_required
 def recipes_list(request):
@@ -19,3 +23,18 @@ def recipe_detail(request, pk):
         "recipe_ingredients": recipe_ingredients
     }
     return render(request, "ledger/recipe_detail.html", ctx)
+
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("recipe_list"))
+        else:
+            ctx = {"Error": "Invalid username or password."}
+            return render(request, "ledger/login.html", ctx)
+    
+    return render(request, "ledger/login.html")
